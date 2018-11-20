4643cc80cc7d20de39c52f280c21dc9fc508f1d4
Tomasz Nowakowski <nowakowski.tomasz88@gmail.com>
2018-11-20 19:15:13 +0100

    Change a topic of content, add option hash, change a view od carusel

--- js/script.js
+++ js/script.js
@@ -4,11 +4,7 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+  contain: true,
+  hash: true,
+  pageDots: false
 });
