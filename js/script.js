d278fbe49d8dc88c881ea9b3ef87fa479722aab9
Tomasz Nowakowski <nowakowski.tomasz88@gmail.com>
2018-11-27 18:40:39 +0100

    Change images, fix hash, add progress bar

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ var flkty = new Flickity( elem, {
   pageDots: false
 });
 
+var progressBar = document.querySelector('.progress-bar');
+
 flkty.on( 'scroll', function( progress ) {
-  console.log( 'Flickity scrolled ' + progress * 100 + '%' )
+  console.log( 'Flickity scrolled ' + progress * 100 + '%' );
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
 
